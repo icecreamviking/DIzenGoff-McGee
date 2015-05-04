39b3d1541d0d0ce5323c14e89e971152210251fe
icecreamviking <icecreamviking@gmail.com>
2015-05-04 19:52:37 -0400

    script fun gen 1

---
+++ scripts/runner.js
@@ -0,0 +1,229 @@
+//=================================home=================================
+
+//about to home
+$('.tempo_home').click(function() {
+  if($('.vanguard').hasClass('vanguard_about')) {
+    $('.vanguard').removeClass('vanguard_about');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_return');
+  }
+});
+
+//menu to home
+
+$('.tempo_home').click(function() {
+  if($('.vanguard').hasClass('vanguard_menu')) {
+    $('.vanguard').removeClass('vanguard_menu');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_return');
+  }
+});
+
+//prorder to home
+
+$('.tempo_home').click(function() {
+  if($('.vanguard').hasClass('vanguard_prorder')) {
+    $('.vanguard').removeClass('vanguard_prorder');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_return');
+  }
+});
+
+//contact to home
+
+$('.tempo_home').click(function() {
+  if($('.vanguard').hasClass('vanguard_contact')) {
+    $('.vanguard').removeClass('vanguard_contact');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_return');
+  }
+});
+
+//==============================about=======================================
+
+//home to about
+
+$('.tempo_about').click(function() {
+  if($('.vanguard').hasClass('vanguard_home')) {
+    $('.vanguard').removeClass('vanguard_home');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_about');
+  }
+});
+
+//menu to about
+
+$('.tempo_about').click(function() {
+  if($('.vanguard').hasClass('vanguard_menu')) {
+    $('.vanguard').removeClass('vanguard_menu');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_about');
+  }
+});
+
+//prorder to about
+
+$('.tempo_about').click(function() {
+  if($('.vanguard').hasClass('vanguard_prorder')) {
+    $('.vanguard').removeClass('vanguard_prorder');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_about');
+  }
+});
+
+//contact to about
+
+$('.tempo_about').click(function() {
+  if($('.vanguard').hasClass('vanguard_contact')) {
+    $('.vanguard').removeClass('vanguard_contact');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_about');
+  }
+});
+
+//====================================menu==============================
+
+
+//home to menu
+
+$('.tempo_menu').click(function() {
+  if($('.vanguard').hasClass('vanguard_home')) {
+    $('.vanguard').removeClass('vanguard_home');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_menu');
+  }
+});
+
+//about to menu
+
+$('.tempo_menu').click(function() {
+  if($('.vanguard').hasClass('vanguard_about')) {
+    $('.vanguard').removeClass('vanguard_about');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_menu');
+  }
+});
+
+//prorder to menu
+
+$('.tempo_menu').click(function() {
+  if($('.vanguard').hasClass('vanguard_prorder')) {
+    $('.vanguard').removeClass('vanguard_prorder');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_menu');
+  }
+});
+
+// contact to menu
+
+$('.tempo_menu').click(function() {
+  if($('.vanguard').hasClass('vanguard_contact')) {
+    $('.vanguard').removeClass('vanguard_contact');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_menu');
+  }
+});
+
+//==================================prorder================================
+
+//home to prorder
+
+$('.tempo_prorder').click(function() {
+  if($('.vanguard').hasClass('vanguard_home')) {
+    $('.vanguard').removeClass('vanguard_home');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_prorder');
+  }
+});
+
+//about to prorder
+
+$('.tempo_prorder').click(function() {
+  if($('.vanguard').hasClass('vanguard_about')) {
+    $('.vanguard').removeClass('vanguard_about');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_prorder');
+  }
+});
+
+//menu to prorder
+
+$('.tempo_prorder').click(function() {
+  if($('.vanguard').hasClass('vanguard_menu')) {
+    $('.vanguard').removeClass('vanguard_menu');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_prorder');
+  }
+});
+
+//contact to prorder
+
+$('.tempo_prorder').click(function() {
+  if($('.vanguard').hasClass('vanguard_contact')) {
+    $('.vanguard').removeClass('vanguard_contact');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_prorder');
+  }
+});
+
+//================================contact=====================================
+
+//home to contact
+
+$('.tempo_contact').click(function() {
+  if($('.vanguard').hasClass('vanguard_home')) {
+    $('.vanguard').removeClass('vanguard_home');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_contact');
+  }
+});
+
+//about to contact
+
+$('.tempo_contact').click(function() {
+  if($('.vanguard').hasClass('vanguard_about')) {
+    $('.vanguard').removeClass('vanguard_about');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_contact');
+  }
+});
+
+// menu to contact
+
+$('.tempo_contact').click(function() {
+  if($('.vanguard').hasClass('vanguard_menu')) {
+    $('.vanguard').removeClass('vanguard_menu');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_contact');
+  }
+});
+
+//prorder to contact
+
+$('.tempo_contact').click(function() {
+  if($('.vanguard').hasClass('vanguard_prorder')) {
+    $('.vanguard').removeClass('vanguard_prorder');
+  }
+  else{
+    $('.vanguard').addClass('vanguard_contact');
+  }
+});
